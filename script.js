39dab7528ca7e2fb5c3b1c522f972deec10635ab
astron-out <cosmos.4618282@gmail.com>
2024-02-03 20:38:19 +0500

    File was updated

--- script.js
+++ script.js
@@ -6,15 +6,68 @@
 
 // const calcAge = birthYear => 2037 - birthYear;
 
-const measureKelvin = function () {
-  const measurment = {
-    type: 'temp',
-    unit: 'celsius',
-    value: prompt('Deegrees celsius: '),
-  };
+// const measureKelvin = function () {
+//   const measurment = {
+//     type: 'temp',
+//     unit: 'celsius',
 
-  const kelvin = measurment.value + 273;
-  return kelvin;
+//     // C) FIX; The wrong version of code: value: prompt('Degrees celsius: ') and it returned value as a string but adding Number() function we fix it!
+//     value: Number(prompt('Degrees celsius: ')),
+//   };
+
+//   // B) FIND
+//   console.table(measurment);
+
+//   const kelvin = measurment.value + 273;
+//   return kelvin;
+// };
+
+// A) IDENTIFY
+// console.log(measureKelvin());
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTemp = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
 };
 
-console.log(measureKelvin());
+// calcTemp([3, 7, 4, 20, 1.5, 'error', 1, 22]);
+const amplitude = calcTemp(temperatures);
+console.log(amplitude);
+
+// Problem 2
+
+const calcTempNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
+};
+
+// calcTemp([3, 7, 4, 20, 1.5, 'error', 1, 22]);
+const amplitudeNew = calcTempNew([2, 4, 6], [8, 11, 15]);
+console.log(amplitudeNew);
